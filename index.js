969383c32a00f77b299cca29a303dbf46449f3c9
nickonyi <nickoluoch1130@gmail.com>
2023-01-19 05:41:29 +0300

    Tic Tac Toe

---
+++ index.js
@@ -0,0 +1,11 @@
+const gridTable = (function() {
+    const board = document.getElementById('board');
+    for (i = 0; i < 9; i++) {
+        const newDiv = document.createElement('div');
+        newDiv.classList.add("board-box");
+        newDiv.style.border = `1px solid black`;
+        board.appendChild(newDiv);
+        board.style.gridTemplateColumns = `repeat(${3},1fr)`;
+        board.style.gridTemplateRows = `repeat(${3},1fr)`;
+    }
+}());
