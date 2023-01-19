a7fe5c2c53e9a9c191888fb2ab82e4efadd4d97b
nickonyi <nickoluoch1130@gmail.com>
2023-01-19 05:51:58 +0300

    Tic Tac Toe

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const gridTable = (function() {
     for (i = 0; i < 9; i++) {
         const newDiv = document.createElement('div');
         newDiv.classList.add("board-box");
-        newDiv.style.border = `1px solid black`;
         board.appendChild(newDiv);
         board.style.gridTemplateColumns = `repeat(${3},1fr)`;
         board.style.gridTemplateRows = `repeat(${3},1fr)`;
