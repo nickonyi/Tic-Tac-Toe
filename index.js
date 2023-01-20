820a4bdf07ff4ff3cc51e4423f0d1fb0dbca532e
nickonyi <nickoluoch1130@gmail.com>
2023-01-20 09:58:39 +0300

    Tablet screen development

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const gridTable = (function() {
+const gameBoard = (function() {
     const board = document.getElementById('board');
+    const gameArr = ["x", "0"];
     for (i = 0; i < 9; i++) {
-        const newDiv = document.createElement('div');
+        var newDiv = document.createElement('div');
         newDiv.classList.add("board-box");
         board.appendChild(newDiv);
         board.style.gridTemplateColumns = `repeat(${3},1fr)`;
         board.style.gridTemplateRows = `repeat(${3},1fr)`;
     }
+    const divs = document.querySelectorAll('.board-box');
+    console.log(divs);
+    divs.forEach(div => div.addEventListener("click", () => {
+        const element = gameArr[0];
+        div.textContent = element;
+    }));
+
 }());
 
 const modalbox = (function() {
