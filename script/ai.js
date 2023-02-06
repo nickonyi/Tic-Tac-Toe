1cfc2218e7583d93d37eec76d5d132aeb4367cc9
nickonyi <nickoluoch1130@gmail.com>
2023-02-06 10:24:06 +0300

    Tic Tac Toe

--- script/ai.js
+++ script/ai.js
@@ -60,6 +60,7 @@ const intRandom = (min, max) => {
 //GRID FUNCTION
 const grid = () => {
     this.cells = new Array(9);
+
     const getCells = () => {
         return cells;
     }
@@ -70,7 +71,139 @@ const grid = () => {
 //grid methods
 const getFreeCellIndices = () => {
     let proto = grid()
-    return Object.assign({}, proto);
+    const getFreeCellIndices = () => {
+        let resultArray = [];
+        for (i = 0; i < this.cells.length; i++) {
+            if (this.cells[i] == 0) {
+                resultArray.push(i);
+            }
+        }
+        return resultArray;
+    }
+    return Object.assign({}, proto, { getFreeCellIndeces });
 };
 
-const newB = getFreeCellIndices();
+const getRowValues = (index) => {
+    let proto = grid();
+    let getRowValues = () => {
+        if (index != 0 && index != 1 && index != 2) {
+            alert('Wrong arguments ');
+            return undefined;
+        }
+        let i = index * 3;
+
+        return this.cells.slice(i, i + 3);
+    }
+
+    return Object.assign({}, proto, { getRowValues });
+}
+
+const getRowIndices = (index) => {
+    let proto = grid();
+    const getRowIndices = () => {
+        if (index !== 0 && index !== 1 && index !== 2) {
+            console.error("Wrong arg for getRowIndices!");
+            return undefined;
+        }
+
+        let row = [];
+        index = index * 3;
+        row.push(index);
+        row.push(index + 1);
+        row.push(index + 2);
+
+        return row;
+    }
+    return Object.assign({}, proto, { getRowIndices });
+}
+
+const getColumnValues = (index) => {
+    const getColumnValues = () => {
+        let i = 0;
+        let column = [];
+
+        for (i = index; this.cells.length; i += 3) {
+            column.push(this.cells[i]);
+        }
+        return column;
+    }
+
+    return Object.assign({}, proto, { getColumnValues });
+}
+
+const getColumnIndices = (index) => {
+    const proto = grid();
+    const getColumnIndices = () => {
+        if (index !== 0 && index !== 1 && index !== 2) {
+            console.log("Wrong arguments for get column indices");
+            return undefined;
+        } else {
+            let column = [];
+            for (i = index; i < this.cells.length; i += 3) {
+                column.push(i);
+            }
+        }
+        return column;
+    }
+    return Object.assign({}, proto, { getColumnIndices });
+}
+
+const getDiagValues = (arg) => {
+    const proto = grid();
+    let cells = [];
+    const getDiagValues = () => {
+        if (arg !== 1 && arg !== 0) {
+            console.log("Wrong arguments for diagonal values");
+            return undefined;
+        } else if (arg === 0) {
+            cells.push(this.cells[0]);
+            cells.push(this.cells[4]);
+            cells.push(this.cells[8]);
+
+        } else {
+            cells.push(this.cells[2]);
+            cells.push(this.cells[4]);
+            cells.push(this.cells[6]);
+        }
+        return cells;
+    }
+
+
+    return Object.assign({}, proto, { getDiagValues });
+}
+
+const getDiagIndices = (arg) => {
+    const proto = grid();
+
+    const getDiagIndices = () => {
+        if (arg !== 1 && arg !== 0) {
+            console.error("Wrong arg for getDiagIndices!");
+            return undefined;
+        } else if (arg === 0) {
+            return [0, 4, 8];
+        } else {
+            return [2, 4, 6];
+        }
+    }
+    return Object.assign({}, proto, { getDiagIndices });
+}
+
+const getFirstWithTwoInARow = (agent) => {
+    const proto = grid();
+    const getFirstWithTwoInARow = () => {
+        if (agent !== computer && agent !== player) {
+            console.error("Function getFirstWithTwoInARow accepts only player or computer as argument.");
+            return undefined;
+        } else {
+            let sum = agent * 2;
+            let freeCells = shuffleArray(this.getFreeCellIndices.getFreeCellIndices());
+            for (i = 0; i < freeCells.length; i++) {
+                for (j = 0; j < 3; j++) {
+
+                }
+            }
+        }
+    }
+
+    return Object.assign({}, proto, getFirstWithTwoInARow);
+}
